380c618d04ea4edb8f8f80717847bbc3e7e76521
Yoshiki Okawa <admin@yoshiki-okawa.com>
2014-07-07 21:00:27 +1200

    Added SharedAssemblyInfo.cs and solution items.

--- ProjectBase/PresentationLayer/WebUI/Properties/AssemblyInfo.cs
+++ ProjectBase/PresentationLayer/WebUI/Properties/AssemblyInfo.cs
@@ -9,26 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebUI")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8d339480-73d9-4056-adfc-3e041ab443c5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
